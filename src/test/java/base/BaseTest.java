ce1ab3eef212fa9c441815c99f75d088fdb6b044
vedantSabban14 <vedantprakash.sabban@cognizant.com>
2026-04-28 09:36:21 +0530

    Added testng.xml

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,9 +2,11 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import utils.configReader;
 import java.time.Duration;
 
@@ -15,9 +17,17 @@ public class BaseTest {
     protected static final String URL = "https://emicalculator.net/";
 
 //    Launch the Browser
+    @Parameters("browser")
     @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
+    public void setUp(String browser) {
+        switch (browser.toLowerCase()){
+            case "chrome": driver = new ChromeDriver();
+            break;
+            case "edge": driver = new EdgeDriver();
+            break;
+            default:
+                throw new IllegalArgumentException("Invalid browser name: " + browser);
+        }
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.manage().window().maximize();
         driver.manage().timeouts()
