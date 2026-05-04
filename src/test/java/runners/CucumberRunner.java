13f5fb05a6baeebbb742cd6dbb9bb96396c1a9e8
chaitanyak7219 <chaitanyachandrakant.kulkarni@cognizant.com>
2026-05-04 15:12:13 +0530

    Updated CucumberRunner

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -1,4 +1,25 @@
 package runners;
 
-public class CucumberRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue     = {"stepDefinitions", "hooks"},   // ← include hooks package
+        plugin   = {
+                "pretty",
+                "html:test-output/cucumber-reports/report.html",
+                "json:test-output/cucumber-reports/report.json"
+        },
+        monochrome = true
+)
+public class CucumberRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios()
+    {
+        return super.scenarios();
+    }
 }
